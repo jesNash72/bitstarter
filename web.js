4f373a099c857675439300ab6257bb1de703c199
Jessica Nash <jessicanashemail@gmail.com>
2013-07-11 20:28:34 +0000

    Debug Mode

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html', 'acsii')});
+ 
+  response.send("Can print this "+fs.readFileSync('index.html', 'utf8')});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
