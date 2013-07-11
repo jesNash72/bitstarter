2377fc960746509f50d19ffe9786d0b4889e7818
Jessica Nash <jessicanashemail@gmail.com>
2013-07-11 20:22:22 +0000

    Try again with i/o

--- web.js
+++ web.js
@@ -3,10 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send("Hello world Again");
-console.log("this Works");
-
-});
+  response.send(fs.readFileSync('index.html', 'acsii')});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
