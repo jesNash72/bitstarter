6d85d620678df357d81c5f02cb289f7f72b51d10
Jessica Nash <jessicanashemail@gmail.com>
2013-07-11 19:50:55 +0000

    Added read in from file

--- web.js
+++ web.js
@@ -1,9 +1,19 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+ var mybuff =  fs.readFile ('./index.html', function(err, data){
+      if(err) throw err;
+      console.log(data);
+
+     })
+;
+  
+
+  response.send(mybuff.toString());
+console.log(mybuff.toString());
+
 });
 
 var port = process.env.PORT || 5000;
