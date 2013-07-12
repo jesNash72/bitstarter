72d97c9c06b709e0270c5a9737958719e1ff1917
Jessica Nash <jessicanashemail@gmail.com>
2013-07-12 00:40:57 +0000

    var change

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
+
+
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
- 
-  response.send("Can print this "+fs.readFileSync('index.html', 'utf8')});
+  var indexFile = fs.readFileSync('index.html', 'ascii'); 
+  response.send(indexFile)});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
